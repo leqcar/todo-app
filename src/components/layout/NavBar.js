ae5356de4e16a5adacdae2b6bf2d344eae3376af
leqcar <leqcar@gmail.com>
2019-07-08 03:19:54 +0800

    changed style

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <Link className="nav-link navbar-brand mb-0 h1" to="/">
         TodoList
-      </a>
+      </Link>
       <button
         className="navbar-toggler"
         type="button"
