c251af96fb59366934a6a3de10aba8ac1b2aa2d4
leqcar <leqcar@gmail.com>
2019-07-08 03:13:05 +0800

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Todos from "./components/Todos";
 import NavBar from "./components/layout/NavBar";
 import AddTodo from "./components/AddTodo";
+import uuid from "uuid";
+import About from "./components/pages/About";
 
 class App extends Component {
   state = {
     todos: [
-      { id: 1, title: "Take out the trash", completed: false },
-      { id: 2, title: "Dinner with wife", completed: false },
-      { id: 3, title: "Meeting with boss", completed: false }
+      { id: uuid.v4(), title: "Take out the trash", completed: false },
+      { id: uuid.v4(), title: "Dinner with wife", completed: false },
+      { id: uuid.v4(), title: "Meeting with boss", completed: false }
     ]
   };
 
@@ -31,9 +34,8 @@ class App extends Component {
 
   handleAddTodoSubmit = title => {
     console.log("New todo: ", title);
-    const newId = this.state.todos.length + 1;
     const todoNew = {
-      id: newId,
+      id: uuid.v4(),
       title,
       completed: false
     };
@@ -42,17 +44,26 @@ class App extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <NavBar />
-        <main className="container">
-          <AddTodo onAddTodoSubmit={this.handleAddTodoSubmit} />
-          <Todos
-            todos={this.state.todos}
-            toggleComplete={this.handleToggleComplete}
-            onDelete={this.handleDelete}
+      <Router>
+        <React.Fragment>
+          <NavBar />
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <main className="container">
+                <AddTodo onAddTodoSubmit={this.handleAddTodoSubmit} />
+                <Todos
+                  todos={this.state.todos}
+                  toggleComplete={this.handleToggleComplete}
+                  onDelete={this.handleDelete}
+                />
+              </main>
+            )}
           />
-        </main>
-      </React.Fragment>
+          <Route path="/about" component={About} />
+        </React.Fragment>
+      </Router>
     );
   }
 }
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <span className="navbar-brand mb-0 h1">TodoList</span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        TodoList
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">
+              Home<span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">
+              About<span className="sr-only">(current)</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
